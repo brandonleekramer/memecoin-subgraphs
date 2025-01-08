d403236cb2c3ed4d70ca3fb16372b6ee19103675
Brandon Kramer <brandonleekramer@gmail.com>
2025-01-07 20:51:11 -0500

    Initialize subgraph

---
+++ cookie-base/src/cookie.ts
@@ -0,0 +1,154 @@
+import {
+  Approval as ApprovalEvent,
+  EnforcedOptionSet as EnforcedOptionSetEvent,
+  MsgInspectorSet as MsgInspectorSetEvent,
+  OFTReceived as OFTReceivedEvent,
+  OFTSent as OFTSentEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+  PeerSet as PeerSetEvent,
+  PreCrimeSet as PreCrimeSetEvent,
+  Transfer as TransferEvent
+} from "../generated/Cookie/Cookie"
+import {
+  Approval,
+  EnforcedOptionSet,
+  MsgInspectorSet,
+  OFTReceived,
+  OFTSent,
+  OwnershipTransferred,
+  PeerSet,
+  PreCrimeSet,
+  Transfer
+} from "../generated/schema"
+
+export function handleApproval(event: ApprovalEvent): void {
+  let entity = new Approval(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.spender = event.params.spender
+  entity.value = event.params.value
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleEnforcedOptionSet(event: EnforcedOptionSetEvent): void {
+  let entity = new EnforcedOptionSet(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity._enforcedOptions = event.params._enforcedOptions
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleMsgInspectorSet(event: MsgInspectorSetEvent): void {
+  let entity = new MsgInspectorSet(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.inspector = event.params.inspector
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOFTReceived(event: OFTReceivedEvent): void {
+  let entity = new OFTReceived(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.guid = event.params.guid
+  entity.srcEid = event.params.srcEid
+  entity.toAddress = event.params.toAddress
+  entity.amountReceivedLD = event.params.amountReceivedLD
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOFTSent(event: OFTSentEvent): void {
+  let entity = new OFTSent(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.guid = event.params.guid
+  entity.dstEid = event.params.dstEid
+  entity.fromAddress = event.params.fromAddress
+  entity.amountSentLD = event.params.amountSentLD
+  entity.amountReceivedLD = event.params.amountReceivedLD
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handlePeerSet(event: PeerSetEvent): void {
+  let entity = new PeerSet(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.eid = event.params.eid
+  entity.peer = event.params.peer
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handlePreCrimeSet(event: PreCrimeSetEvent): void {
+  let entity = new PreCrimeSet(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.preCrimeAddress = event.params.preCrimeAddress
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.value = event.params.value
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
